51a38faaa082e457979fc03899438bfd2328eeed
Горожанкин Вадим <vadim.gorozhankin@gazprombank.ru>
2024-04-29 18:56:42 +0300

    game + task 9

---
+++ src/com/javarush/games/minesweeper/GameObject.java
@@ -0,0 +1,11 @@
+package com.javarush.games.minesweeper;
+
+public class GameObject {
+    public int x = 0;
+    public int y = 0;
+
+    public GameObject(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
